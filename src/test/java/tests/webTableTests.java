4a7300c9fcc35acfc979d4c0377f4fb7d6e75eca
pgligan <paul.gligan@rabobank.com>
2025-04-22 23:34:39 +0300

    Lesson 10 - Web Tables test refactored

--- src/test/java/tests/webTableTests.java
+++ src/test/java/tests/webTableTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -8,10 +9,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import HelperMethods.elementsMethod;
 
 public class webTableTests {
 
     public WebDriver driver;
+    public elementsMethod elementsMethod;
 
     @Test
     public void autMethod() {
@@ -19,6 +22,8 @@ public class webTableTests {
         driver = new ChromeDriver();
 //  Facem browserul in modul maximize
         driver.manage().window().maximize();
+        elementsMethod = new elementsMethod(driver);
+
 //  Accesam o pagina web
         driver.get("https://demoqa.com/");
 
@@ -27,46 +32,49 @@ public class webTableTests {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,400)");
 
-//  declaram un element
+//  declaram lista elemente si alegem Elements
         WebElement elementsField = driver.findElement(By.xpath("//h5[text()='Elements']"));
-        elementsField.click();
+
+        List<WebElement> mainMenuElement = new ArrayList<WebElement>();
+        mainMenuElement.add(elementsField);
+
+        elementsMethod.clickElements(elementsField);
 
         WebElement webTableElement = driver.findElement(By.xpath("//span[text()='Web Tables']"));
-        webTableElement.click();
+        elementsMethod.clickElements(webTableElement);
 
         List<WebElement> TableElements = driver.findElements(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -even' or @class='rt-tr -odd']"));
         Integer actualTableSize = TableElements.size();
 
-
         WebElement addNewTableItemElement = driver.findElement(By.id("addNewRecordButton"));
-        addNewTableItemElement.click();
+        elementsMethod.clickElements(addNewTableItemElement);
 
         WebElement firstNameElement = driver.findElement(By.id("firstName"));
         String firstNameValue = "FirsNameInput1";
-        firstNameElement.sendKeys(firstNameValue);
+        elementsMethod.sendKeys(firstNameElement,firstNameValue);
 
         WebElement lastNameElement = driver.findElement(By.id("lastName"));
         String lastNameValue = "LastNameInput1";
-        lastNameElement.sendKeys(lastNameValue);
+        elementsMethod.sendKeys(lastNameElement,lastNameValue);
 
         WebElement userEmailElement = driver.findElement(By.id("userEmail"));
         String userEmailValue = "userEmailInput1@example.com";
-        userEmailElement.sendKeys(userEmailValue);
+        elementsMethod.sendKeys(userEmailElement,userEmailValue);
 
         WebElement ageElement = driver.findElement(By.id("age"));
         String ageValue = "33";
-        ageElement.sendKeys(ageValue);
+        elementsMethod.sendKeys(ageElement,ageValue);
 
         WebElement salaryElement = driver.findElement(By.id("salary"));
         String salaryValue = "10000";
-        salaryElement.sendKeys(salaryValue);
+        elementsMethod.sendKeys(salaryElement,salaryValue);
 
         WebElement departmentElement = driver.findElement(By.id("department"));
         String departmentValue = "Deprtement1";
-        departmentElement.sendKeys(departmentValue);
+        elementsMethod.sendKeys(departmentElement,departmentValue);
 
         WebElement clickSubmitButtonElement = driver.findElement(By.id("submit"));
-        clickSubmitButtonElement.click();
+        elementsMethod.clickElements(clickSubmitButtonElement);
 
         List<WebElement> ExpectedTableElements = driver.findElements(By.xpath("//div[@class='rt-tbody']/div/div[@class='rt-tr -even' or @class='rt-tr -odd']"));
 
