63197d3cb54eb69595af1ca2079a359c921deb0b
pgligan <paul.gligan@rabobank.com>
2025-04-22 23:05:29 +0300

    Lesson 10 - Browser windows helper and test refactored

--- src/test/java/HelperMethods/windowsMethods.java
+++ src/test/java/HelperMethods/windowsMethods.java
@@ -1,4 +1,25 @@
 package HelperMethods;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class windowsMethods {
+    WebDriver driver;
+
+    public windowsMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchToSecondTab() {
+        List<String> tabList = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabList.get(1));
+    }
+
+    public void switchToMainTab() {
+        driver.close();
+        List<String> tabList = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabList.get(0));
+    }
 }
