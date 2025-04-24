388a077f536f7fae2991f1d1dc0d8f247ac2bdb7
pgligan <paul.gligan@rabobank.com>
2025-04-25 01:09:32 +0300

    Lesson 11 - Completed without the 4 pages remaining as homework

--- src/test/java/HelperMethods/elementsMethod.java
+++ src/test/java/HelperMethods/elementsMethod.java
@@ -1,24 +1,33 @@
 package HelperMethods;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class elementsMethod {
 
     WebDriver driver;
+    Actions actions;
 
     public elementsMethod(WebDriver driver) {
+
         this.driver = driver;
+        this.actions = new Actions(driver);
     }
 
     public void clickElements(WebElement element) {
+
         element.click();
     }
 
     public void sendKeys(WebElement element, String value) {
+        waitElementVisibility(element);
         element.sendKeys(value);
     }
 
@@ -46,7 +55,24 @@ public class elementsMethod {
         }
     }
 
+    public void fillWithActions(WebElement element, String value) {
+        actions.sendKeys(value).build().perform();
+        waitElementVisibility(element);
+        actions.sendKeys(Keys.ENTER).build().perform();
+        //merge si cu .build.perform()
+    }
 
+    public void waitElementVisibility(WebElement element) {
+        //explicit wait for element visibility
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
 
-
+    public void fillMultipleValues(WebElement element, List<String> list) {
+        for(String value: list){
+//            fillWithActions(element, values);
+            element.sendKeys(value);
+            element.sendKeys(Keys.ENTER);
+       }
+    }
 }
