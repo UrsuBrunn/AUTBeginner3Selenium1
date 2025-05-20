53bdf6e8f88d4f11306274bdd349a75e97d00661
pgligan <paul.gligan@rabobank.com>
2025-05-20 21:05:33 +0300

    Lesson 15 - CICD - Part1 - XML config file WIP

--- src/test/java/configFile/ConfigFile.java
+++ src/test/java/configFile/ConfigFile.java
@@ -1,4 +1,26 @@
 package configFile;
 
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 public class ConfigFile {
+
+    //facem o metoda generica care sa deserializeze un XML file sisa imim returneze o bucata pe care o doresc
+    //metoda generica
+    public static <T> T createConfigNode(Class<T> klass) {
+        try {
+
+            JAXBContext jaxbContext = JAXBContext.newInstance(klass);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            return klass.cast(unmarshaller.unmarshal(new File("src/test/resources/configFile/sharedDataConfig.xml")));
+
+        }
+        catch (JAXBException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
 }
