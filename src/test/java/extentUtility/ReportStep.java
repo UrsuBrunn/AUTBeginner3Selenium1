795e12180841ddc3354714b0e849f9a84de1680f
pgligan <paul.gligan@rabobank.com>
2025-06-19 09:07:58 +0300

    Lesson 20 - Test execution report utility + screenshot

---
+++ src/test/java/extentUtility/ReportStep.java
@@ -0,0 +1,7 @@
+package extentUtility;
+
+public class ReportStep {
+    public static String INFO_STEP = "info";
+    public static String ERROR_STEP = "error";
+    public static String PASS_STEP = "pass";
+}
